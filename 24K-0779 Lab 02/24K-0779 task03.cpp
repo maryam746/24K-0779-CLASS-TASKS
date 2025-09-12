5eef7e9bc259304556c413910ec014989fba8bb9
Maryam-Saleem <solivagantmaryam26@gmail.com>
2025-09-12 14:53:07 +0500

    Create 24K-0779 task03.cpp

---
+++ 24K-0779 Lab 02/24K-0779 task03.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int teams;
+    cout << "Enter number of teams: ";
+    cin >> teams;
+
+    int** scores = new int*[teams];
+    int* teamSizes = new int[teams];
+
+    for (int i = 0; i < teams; i++) {
+        cout << "Enter number of players in team " << i + 1 << ": ";
+        cin >> teamSizes[i];
+        scores[i] = new int[teamSizes[i]];
+    }
+
+    for (int i = 0; i < teams; i++) {
+        cout << "Enter scores for team " << i + 1 << ": ";
+        for (int j = 0; j < teamSizes[i]; j++) {
+            cin >> scores[i][j];
+        }
+    }
+
+    for (int i = 0; i < teams; i++) {
+        int sum = 0;
+        for (int j = 0; j < teamSizes[i]; j++) {
+            sum += scores[i][j];
+        }
+        cout << "Sum of scores for team " << i + 1 << ": " << sum << endl;
+    }
+
+    for (int i = 0; i < teams; i++) {
+        delete[] scores[i];
+    }
+    delete[] scores;
+    delete[] teamSizes;
+
+    return 0;
+}
